a62ed2f03a3a887dad91b732bf2106102a4929ab
seokjin shin <sars21@hanmail.net>
2022-08-03 00:56:34 +0900

    리뉴얼

---
+++ src/main/java/com/shin/whatshoes/WhatshoesApplication.java
@@ -0,0 +1,13 @@
+package com.shin.whatshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatshoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatshoesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shin/whatshoes/WhatshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shin.whatshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatshoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
